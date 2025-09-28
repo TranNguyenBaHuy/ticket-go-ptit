31941a633db72d91489862d9948a864e69ce3391
Trần Nguyễn Bá Huy <trannguyenbahuy2908@gmail.com>
2025-09-28 21:52:03 +0700

    update route for All Event

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,13 +3,14 @@ import About from "../pages/About";
 import Products from "../pages/MyTickets";
 import DefaultLayout from "../components/Layouts/DefaultLayout/DefaultLayout";
 import Contact from "../pages/Contact";
+import AllEvents from "../pages/AllEvents";
 
 const publicRoutes = [
   { path: "/", component: Home, layout: DefaultLayout },
   { path: "/about", component: About, layout: DefaultLayout },
   { path: "/my-tickets", component: Products, layout: DefaultLayout },
   { path: "/contact", component: Contact, layout: DefaultLayout },
-  { path: "/all-events", component: Contact, layout: DefaultLayout },
+  { path: "/all-events", component: AllEvents, layout: DefaultLayout },
 ];
 
 const privateRoutes = [];
