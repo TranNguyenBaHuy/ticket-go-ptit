53cb392ffdb393535bd670e8a6b5b2961d0fa606
Trần Nguyễn Bá Huy <trannguyenbahuy2908@gmail.com>
2025-11-18 22:02:27 +0700

    feat: implement category fiter for AllEvent page

--- src/pages/AllEvents.tsx
+++ src/pages/AllEvents.tsx
@@ -11,6 +11,8 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import CategoryFilterBar from "@/components/Layouts/Client/CategoryFilterBar";
+import { categories } from "@/constants/data/categories";
 
 const AllEvents = () => {
   const [events, setEvents] = useState<Event[]>([]);
@@ -51,83 +53,89 @@ const AllEvents = () => {
   }, [categoryName, searchQuery, currentPage]);
 
   return (
-    <div className="min-h-screen bg-[#000]">
-      <div className="mx-5 lg:mx-auto max-w-[1250px]">
-        {/* header */}
-        <div className="text-white container py-6">
-          <p className="text-[#2dc275] font-semibold">Kết quả tìm kiếm:</p>
+    <>
+      <CategoryFilterBar data={categories} />
+      <div className="min-h-screen bg-[#000]">
+        <div className="mx-5 lg:mx-auto max-w-[1250px]">
+          {/* header */}
+          <div className="text-white container py-6">
+            <p className="text-[#2dc275] font-semibold">Kết quả tìm kiếm:</p>
+          </div>
+          {/* seach results section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+            {loading
+              ? Array.from({ length: 8 }).map((_, index) => (
+                  <div
+                    key={index}
+                    className="bg-[#3f3f46] rounded-xl aspect-[16/9] animate-pulse"
+                  ></div>
+                ))
+              : events.map((event, index) => (
+                  <EventCard
+                    key={event.id ?? index}
+                    event={event}
+                    price={getDisplayPrice(event.ticketTypes)}
+                  />
+                ))}
+          </div>
         </div>
-        {/* seach results section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-          {loading
-            ? Array.from({ length: 8 }).map((_, index) => (
-              <div
-                key={index}
-                className="bg-[#3f3f46] rounded-xl aspect-[16/9] animate-pulse"
-              ></div>
-            ))
-            : events.map((event, index) => (
-              <EventCard
-                key={event.id ?? index}
-                event={event}
-                price={getDisplayPrice(event.ticketTypes)}
-              />
-            ))}
-        </div>
-      </div>
 
-      {/* shadcn PAGINATION  */}
-      {totalPages > 1 && (
-        <div className="flex justify-center py-8">
-          <Pagination>
-            <PaginationContent className="text-white">
-              {/* prev btn */}
-              <PaginationItem>
-                <PaginationPrevious
-                  href="#"
-                  onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
-                  className={`${currentPage === 1
-                    ? "opacity-40 pointer-events-none"
-                    : "hover:bg-blue-600 hover:text-white"
-                    } bg-[#3f3f46] text-white border border-gray-600`}
-                />
-              </PaginationItem>
-
-              {/* page nums */}
-              {Array.from({ length: totalPages }, (_, i) => (
-                <PaginationItem key={i}>
-                  <PaginationLink
+        {/* shadcn PAGINATION  */}
+        {totalPages > 1 && (
+          <div className="flex justify-center py-8">
+            <Pagination>
+              <PaginationContent className="text-white">
+                {/* prev btn */}
+                <PaginationItem>
+                  <PaginationPrevious
                     href="#"
-                    isActive={currentPage === i + 1}
-                    onClick={() => setCurrentPage(i + 1)}
-                    className={`${currentPage === i + 1
-                      ? "bg-blue-500 text-white border-blue-500"
-                      : "bg-[#3f3f46] text-gray-300 hover:bg-blue-600 hover:text-white border border-gray-600"
-                      }`}
-                  >
-                    {i + 1}
-                  </PaginationLink>
-                </PaginationItem>
-              ))}
-
-              {/* next btn */}
-              <PaginationItem>
-                <PaginationNext
-                  href="#"
-                  onClick={() =>
-                    setCurrentPage((p) => Math.min(p + 1, totalPages))
-                  }
-                  className={`${currentPage === totalPages
-                    ? "opacity-40 pointer-events-none"
-                    : "hover:bg-blue-600 hover:text-white"
+                    onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+                    className={`${
+                      currentPage === 1
+                        ? "opacity-40 pointer-events-none"
+                        : "hover:bg-blue-600 hover:text-white"
                     } bg-[#3f3f46] text-white border border-gray-600`}
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        </div>
-      )}
-    </div>
+                  />
+                </PaginationItem>
+
+                {/* page nums */}
+                {Array.from({ length: totalPages }, (_, i) => (
+                  <PaginationItem key={i}>
+                    <PaginationLink
+                      href="#"
+                      isActive={currentPage === i + 1}
+                      onClick={() => setCurrentPage(i + 1)}
+                      className={`${
+                        currentPage === i + 1
+                          ? "bg-blue-500 text-white border-blue-500"
+                          : "bg-[#3f3f46] text-gray-300 hover:bg-blue-600 hover:text-white border border-gray-600"
+                      }`}
+                    >
+                      {i + 1}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))}
+
+                {/* next btn */}
+                <PaginationItem>
+                  <PaginationNext
+                    href="#"
+                    onClick={() =>
+                      setCurrentPage((p) => Math.min(p + 1, totalPages))
+                    }
+                    className={`${
+                      currentPage === totalPages
+                        ? "opacity-40 pointer-events-none"
+                        : "hover:bg-blue-600 hover:text-white"
+                    } bg-[#3f3f46] text-white border border-gray-600`}
+                  />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
